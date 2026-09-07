944db98e640a6beff42feb5e5c527edc77f9d8c6
Allen Xu <allxu@nvidia.com>
2026-09-07 11:19:05 +0800

    Add Delta column mapping predicate coverage
    Signed-off-by: Allen Xu <allxu@nvidia.com>

--- integration_tests/src/main/python/delta_lake_test.py
+++ integration_tests/src/main/python/delta_lake_test.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
+
 import pytest
 from pyspark.sql import Row
 from asserts import assert_gpu_fallback_collect, assert_gpu_and_cpu_are_equal_collect, \
@@ -940,6 +942,74 @@ def test_delta_read_column_mapping(spark_tmp_path, reader_confs, mapping, enable
     with_cpu_session(create_delta, conf=confs)
     assert_gpu_and_cpu_are_equal_collect(lambda spark: spark.read.format("delta").load(data_path),
                                          conf=confs)
+
+
+@allow_non_gpu(*delta_meta_allow)
+@delta_lake
+@ignore_order(local=True)
+@pytest.mark.skipif(is_databricks_runtime(), reason="OSS Delta column-mapping scan path")
+@pytest.mark.skipif(not gpu_supports_delta_dv_scan(),
+                    reason="GPU Delta deletion vector scan support is required")
+def test_delta_column_mapping_predicate_pushdown_with_deletion_vector(spark_tmp_path):
+    data_path = spark_tmp_path + "/DELTA_DATA"
+    cpu_path = data_path + "/CPU"
+    gpu_path = data_path + "/GPU"
+    conf = {
+        "spark.databricks.delta.properties.defaults.columnMapping.mode": "name",
+        "spark.databricks.delta.properties.defaults.minReaderVersion": "2",
+        "spark.databricks.delta.properties.defaults.minWriterVersion": "5",
+        "spark.databricks.delta.properties.defaults.enableDeletionVectors": "true",
+        "spark.databricks.delta.delete.deletionVectors.persistent": "true",
+        "spark.sql.parquet.fieldId.read.enabled": "true",
+        "spark.rapids.sql.format.parquet.reader.type": "PERFILE",
+    }
+
+    def setup_table(spark, path):
+        spark.createDataFrame(
+            [(0, None), (1, "one"), (2, None), (3, "three"), (4, "four")],
+            ["id", "payload"]) \
+            .write.format("delta").mode("overwrite").save(path)
+        deleted = spark.sql(
+            f"DELETE FROM delta.`{path}` WHERE id = 4").collect()[0][0]
+        assert deleted == 1
+
+    with_cpu_session(lambda spark: setup_table(spark, cpu_path), conf=conf)
+    with_cpu_session(lambda spark: setup_table(spark, gpu_path), conf=conf)
+
+    def assert_column_mapping_and_dv(spark, path):
+        schema_strings = spark.read.text(path + "/_delta_log/*.json") \
+            .selectExpr("get_json_object(value, '$.metaData.schemaString') AS schema") \
+            .where("schema IS NOT NULL") \
+            .collect()
+        assert len(schema_strings) == 1
+        fields = json.loads(schema_strings[0][0])["fields"]
+        physical_names = {
+            field["name"]: field["metadata"]["delta.columnMapping.physicalName"]
+            for field in fields
+        }
+        assert set(physical_names) == {"id", "payload"}
+        assert all(logical_name != physical_name
+                   for logical_name, physical_name in physical_names.items())
+        dv_adds = spark.read.json(path + "/_delta_log/*.json") \
+            .where("add.deletionVector IS NOT NULL").count()
+        assert dv_adds > 0, f"Expected a deletion-vector AddFile in {path}"
+
+    with_cpu_session(
+        lambda spark: assert_column_mapping_and_dv(spark, cpu_path), conf=conf)
+    with_cpu_session(
+        lambda spark: assert_column_mapping_and_dv(spark, gpu_path), conf=conf)
+
+    def filtered_read(spark):
+        path = gpu_path if spark.conf.get("spark.rapids.sql.enabled") == "true" else cpu_path
+        return spark.read.format("delta").load(path) \
+            .where("id IN (1, 2, 3) AND payload IS NOT NULL") \
+            .select("id", "payload")
+
+    assert_cpu_and_gpu_are_equal_collect_with_capture(
+        filtered_read,
+        exist_classes="GpuFileSourceScanExec",
+        conf=conf,
+        require_non_empty=True)
 
 
 @allow_non_gpu(*delta_meta_allow)
